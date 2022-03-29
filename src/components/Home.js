9aca7b0911e284b784c9a292d740ef8b4eccdf23
Bhat Shaqran <bhatshakran@gmail.com>
2022-03-29 15:41:50 +0530

    Login with spotify added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Header from "./Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  console.log(process.env.REACT_APP_CLIENT_ID);
+  const handleLogin = () => {
+    window.location = `${process.env.REACT_APP_AUTHORIZE_URL}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
+  };
+  return (
+    <div className="login bg-slate-200 text-center h-screen">
+      <Header />
+      <button
+        type="submit"
+        onClick={handleLogin}
+        className="bg-blue-600 p-2 text-white"
+      >
+        Login using spotify
+      </button>
+    </div>
+  );
 };
 
 export default Home;
