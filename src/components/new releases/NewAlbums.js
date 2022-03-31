b68239af121fdc61219a7c46783743ee087bcdb4
Bhat Shaqran <bhatshakran@gmail.com>
2022-03-31 18:40:09 +0530

    New album releases made fully responsive

--- src/components/new releases/NewAlbums.js
+++ src/components/new releases/NewAlbums.js
@@ -4,34 +4,78 @@ import { useSelector } from "react-redux";
 
 const NewAlbums = () => {
   let newalbums = useSelector((state) => state.apidata.newalbums);
-  let [slidesToShow, setSlides] = useState(window.innerWidth > 1024 ? 4 : 2);
-
-  const releasesSettings = {
+  let initialVal;
+  let releasesSettings = {
     responsive: [
+      {
+        breakpoint: 2800,
+        settings: {
+          slidesToShow: 6,
+        },
+      },
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
         },
       },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
+      // {
+      //   breakpoint: 600,
+      //   settings: {
+      //     slidesToShow: 1,
+      //   },
+      // },
     ],
   };
 
+  // Set initial no of slide
+  (() => {
+    let { innerWidth: width } = window;
+    const { responsive } = releasesSettings;
+
+    if (width < responsive[0].breakpoint && width > responsive[1].breakpoint) {
+      initialVal = responsive[0].settings.slidesToShow;
+    } else if (
+      width < responsive[1].breakpoint &&
+      width > responsive[2].breakpoint
+    ) {
+      initialVal = responsive[1].settings.slidesToShow;
+    } else if (width < responsive[2].breakpoint) {
+      initialVal = responsive[2].settings.slidesToShow;
+    } else {
+      initialVal = 6;
+    }
+  })();
+
+  // slidesto show state value
+
+  let [slidesToShow, setSlides] = useState(initialVal);
+
+  // album cards renderer function
   const albumcardsrenderer = () => {
     let { innerWidth: width } = window;
     const { responsive } = releasesSettings;
-    if (width < responsive[0].breakpoint) {
+    if (width < responsive[0].breakpoint && width > responsive[1].breakpoint) {
       setSlides(responsive[0].settings.slidesToShow);
+    } else if (
+      width < responsive[1].breakpoint &&
+      width > responsive[2].breakpoint
+    ) {
+      setSlides(responsive[1].settings.slidesToShow);
+    } else if (width < responsive[2].breakpoint) {
+      setSlides(responsive[2].settings.slidesToShow);
     } else {
-      setSlides(4);
+      setSlides(6);
     }
   };
+
+  // use layout effect
 
   useLayoutEffect(() => {
     const debouncedrenderer = _.debounce(albumcardsrenderer, 100);
@@ -42,16 +86,15 @@ const NewAlbums = () => {
     };
   }, []);
 
+  // return statement
+
   return (
     <div className="max-w-full mx-8 mt-12 h-80">
       <div className="flex gap-2 releases_container">
         {newalbums.map((album, id) => {
           if (id < slidesToShow) {
             return (
-              <div
-                key={album.id}
-                className="flex flex-col w-auto h-auto p-4 text-white rounded-md lg:h-72 bg-card lg:w-52"
-              >
+              <div key={album.id} className="card">
                 <img
                   src={album.images[1].url}
                   alt={album.name}
