2c756037f08ea8679a7ef19ed289fff628503e03
Bhat Shaqran <bhatshakran@gmail.com>
2022-04-06 15:11:49 +0530

    Render iconbar when screen size is less than 768px

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,8 @@ import React, { useEffect, useLayoutEffect, useState } from "react";
 import logo from "../img/beatastic.svg";
 import { RiCompassDiscoverLine } from "react-icons/ri";
 import { GiLoveSong } from "react-icons/gi";
-import { BiAlbum } from "react-icons/bi";
+import { BiAlbum, BiUserCircle } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   let initialVal;
@@ -29,19 +30,43 @@ const Navbar = () => {
     };
   }, []);
 
-  return (
-    <div className="sidebar">
-      {/* logo here */}
-      <div className="mx-auto w-28 h-28">
-        <img src={logo} alt="" />
-      </div>
-      {/* music section */}
-
-      {iconBar ? (
-        <div>Loading..</div>
-      ) : (
-        <>
-          {" "}
+  {
+    if (iconBar)
+      return (
+        <div className="relative iconbar">
+          {/* logo here */}
+          <div className="w-12 h-12 mx-auto">
+            <Link to="/dashboard">
+              <img src={logo} alt="" />
+            </Link>
+          </div>
+          <div className="flex flex-col items-center mt-16 cursor-pointer gap-y-8 ">
+            <div className="text-4xl hover:text-yellow-400">
+              <Link to="/dashboard">
+                <RiCompassDiscoverLine />
+              </Link>
+            </div>
+            <div className="text-4xl hover:text-yellow-400">
+              <GiLoveSong />
+            </div>
+            <div className="text-4xl hover:text-yellow-400">
+              <BiAlbum />
+            </div>
+            <div className="text-4xl hover:text-yellow-400"></div>
+          </div>
+          <div className="absolute text-4xl -translate-x-1/2 cursor-pointer bottom-5 hover:text-yellow-400 left-1/2">
+            <BiUserCircle />
+          </div>
+        </div>
+      );
+    else
+      return (
+        <div className="sidebar">
+          {/* logo here */}
+          <div className="mx-auto w-28 h-28">
+            <img src={logo} alt="" />
+          </div>
+          {/* music section */}{" "}
           <section className="mt-16 music">
             <h3 className="text-gray-400 font-notulen">Music</h3>
             <ul className="text-white ">
@@ -51,6 +76,7 @@ const Navbar = () => {
               <li>Artists</li>
             </ul>
           </section>
+          {/* playlist section */}
           <section className="mt-16 playlists">
             <h3 className="text-gray-400 font-notulen">Playlists</h3>
             <ul className="text-white">
@@ -60,10 +86,9 @@ const Navbar = () => {
               <li>Bolly</li>
             </ul>
           </section>
-        </>
-      )}
-    </div>
-  );
+        </div>
+      );
+  }
 };
 
 export default Navbar;
