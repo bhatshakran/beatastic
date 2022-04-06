d1dd4506b282562f6066874d149c487fbd67edf1
Bhat Shaqran <bhatshakran@gmail.com>
2022-04-06 12:22:15 +0530

    Change side bar size based on the viewport width

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,32 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import logo from "../img/beatastic.svg";
-import { getUserAlbums } from "../redux/features/spotifyslice";
+import { RiCompassDiscoverLine } from "react-icons/ri";
+import { GiLoveSong } from "react-icons/gi";
+import { BiAlbum } from "react-icons/bi";
 
 const Navbar = () => {
-  const dispatch = useDispatch();
+  let initialVal;
+  let { innerWidth: width } = window;
 
-  useEffect(() => {
-    // dispatch(getUserAlbums());
+  if (width < 768) {
+    initialVal = true;
+  } else initialVal = false;
+
+  let [iconBar, setIconBar] = useState(initialVal);
+
+  const changeBar = () => {
+    let { innerWidth: width } = window;
+    if (width <= 768) {
+      setIconBar(true);
+    } else setIconBar(false);
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener("resize", changeBar);
+    // Cleanup
+    return (_) => {
+      window.removeEventListener("resize", changeBar);
+    };
   }, []);
 
   return (
@@ -17,25 +36,32 @@ const Navbar = () => {
         <img src={logo} alt="" />
       </div>
       {/* music section */}
-      <section className="mt-16 music">
-        <h3 className="text-gray-400 font-notulen">Music</h3>
-        <ul className="text-white ">
-          <li>Discover</li>
-          <li>Songs</li>
-          <li>Albums</li>
-          <li>Artists</li>
-        </ul>
-      </section>
-      {/* playlists section */}
-      <section className="mt-16 playlists">
-        <h3 className="text-gray-400 font-notulen">Playlists</h3>
-        <ul className="text-white">
-          <li>Vibes</li>
-          <li>Drill</li>
-          <li>Calm</li>
-          <li>Bolly</li>
-        </ul>
-      </section>
+
+      {iconBar ? (
+        <div>Loading..</div>
+      ) : (
+        <>
+          {" "}
+          <section className="mt-16 music">
+            <h3 className="text-gray-400 font-notulen">Music</h3>
+            <ul className="text-white ">
+              <li>Discover</li>
+              <li>Songs</li>
+              <li>Albums</li>
+              <li>Artists</li>
+            </ul>
+          </section>
+          <section className="mt-16 playlists">
+            <h3 className="text-gray-400 font-notulen">Playlists</h3>
+            <ul className="text-white">
+              <li>Vibes</li>
+              <li>Drill</li>
+              <li>Calm</li>
+              <li>Bolly</li>
+            </ul>
+          </section>
+        </>
+      )}
     </div>
   );
 };
